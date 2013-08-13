fc4fa27ee7f941d9e5c9d06209215fc1a5f9155d
Shady <shady@shadyproject.net>
2013-08-12 22:06:52 -0700

    Create and print some entries

--- lottery/lottery/main.m
+++ lottery/lottery/main.m
@@ -7,14 +7,37 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LotteryEntry.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        NSDate* now = [[NSDate alloc] init];
+        NSCalendar* cal = [NSCalendar currentCalendar];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDateComponents* weekComponents = [[NSDateComponents alloc] init];
+        
+        srandom((unsigned)time(NULL));
+        
+        NSMutableArray* array = [[NSMutableArray alloc] init];
+        
+        for (int i=0; i < 10; i+=1) {
+            [weekComponents setWeek:i];
+            
+            NSDate* weeksFromNow = [cal dateByAddingComponents:weekComponents toDate:now options:0];
+            
+            LotteryEntry* entry = [[LotteryEntry alloc] init];
+            [entry prepareRandomNumbers];
+            [entry setEntryDate:weeksFromNow];
+            
+            [array addObject:entry];
+        }
+        
+        for (LotteryEntry* printMe in array) {
+            NSLog(@"%@", printMe);
+        }
+        
         
     }
     return 0;
