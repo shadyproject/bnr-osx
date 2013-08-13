d9f36bc012ca9da4bdf5b1252f47a9b8b997b12c
Shady <shady@shadyproject.net>
2013-08-12 22:06:30 -0700

    initial implmentation

--- lottery/lottery/LotteryEntry.m
+++ lottery/lottery/LotteryEntry.m
@@ -10,4 +10,24 @@
 
 @implementation LotteryEntry
 
+-(void)prepareRandomNumbers{
+    firstNumber = ((int)random() % 100)+1;
+    secondNumber = ((int)random() % 100)+1;
+}
+
+-(void)setEntryDate:(NSDate *)date{
+    entryDate = date;
+}
+
+-(NSDate*)entryDate{
+    return entryDate;
+}
+
+-(int)firstNumber{
+    return firstNumber;
+}
+
+-(int)secondNumber{
+    return secondNumber;
+}
 @end
